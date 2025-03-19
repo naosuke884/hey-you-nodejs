97f8a15b11c9137d593b7966a1ae98d544ab15d9
naosuke884 <884naoki.general@gmail.com>
2025-03-19 22:03:50 +0000

    chore: base

---
+++ app/chat/[id]/page.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+const ChatPage: FC = async () => {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <p>これは のchat ページです。</p>
+    </div>
+  );
+};
+
+export default ChatPage;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const TopPage: FC = () => {
+  return (
+    <div>
+      <h1>Top Page</h1>
+    </div>
+  );
+};
+
+export default TopPage;
